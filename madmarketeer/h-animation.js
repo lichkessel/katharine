839eb42458e4a0de87df0b0f9772615cd718d968
Alexander Nedosekin <anedosekin@gmail.com>
2016-08-11 22:23:36 +0300

    initial commit

---
+++ madmarketeer/h-animation.js
@@ -0,0 +1,25 @@
+$(function() {
+  function animate() {
+    var frame = $( ".h-container .h-dynamic" )
+    frame
+    .delay(2000)
+    .animate({
+      width:20
+    },500, function() {
+      var child = frame.children('.h-active');
+      child.removeClass('h-active');
+      var next = child.next();
+      if(!next.length) {
+        next = frame.children().first()
+      }
+      next.addClass('h-active');
+    })
+    .delay(100)
+    .animate({
+      width:400
+    },500, function() {
+       animate();
+    });
+  }
+  animate();
+});
